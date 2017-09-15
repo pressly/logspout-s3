aa82c88b797ca8030524966dc82ca334bba37db5
Peter Kieltyka <peter.kieltyka@pressly.com>
2017-09-15 14:04:48 -0400

    Add NOTE about main.go

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/gliderlabs/logspout/router"
 )
 
+// NOTE: this main() function is not included in the compiled Docker
+// container application. The program below is just a test program to
+// verify the code in modules.go which gets packaged with gliderlabs/logspout.
 func main() {
 	os.Setenv("AWS_ACCESS_KEY", "...")
 	os.Setenv("AWS_SECRET_KEY", "...")
